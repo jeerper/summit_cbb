5013e937e706471516c7051aaca17b783887beed
liuyuan <405653510@qq.com>
2018-12-10 10:29:47 +0800

    commit

--- cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/MainAction.java
+++ cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/MainAction.java
@@ -2,27 +2,21 @@ package com.summit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableEurekaClient
-@EnableDiscoveryClient
-@ImportResource(locations= {"classpath:applicationContext.xml"})
+//@EnableZuulProxy
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+//@ImportResource(locations= {"classpath:applicationContext.xml"})
 public class MainAction {
 
     public static void main(String[] args) {
         SpringApplication.run(MainAction.class, args);
     }
 
-    @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
     
 }
