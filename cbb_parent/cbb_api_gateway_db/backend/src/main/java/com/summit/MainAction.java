88b571cd0a76395c7fac4324ef4fc2b4f9dc3332
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-18 16:06:42 +0800

    commit

--- cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/MainAction.java
+++ cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/MainAction.java
@@ -2,10 +2,11 @@ package com.summit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //@EnableZuulProxy
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class MainAction {
 
