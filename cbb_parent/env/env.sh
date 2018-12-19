76912aad8d26b096115cb0a29b5ccb5dd5338e15
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-19 10:07:39 +0800

    提交

--- cbb_parent/env/env.sh
+++ cbb_parent/env/env.sh
@@ -20,6 +20,6 @@ spring_boot_admin_username="ucp"
 #性能监控控制台密码
 spring_boot_admin_password="Summit2018"
 #注册中心IP
-Registry_Center_IP="172.17.2.226"
+Registry_Center_IP="192.168.140.155"
 #注册中心URL
 Registry_Center_URL="http://${spring_boot_admin_username}:${spring_boot_admin_password}@${Registry_Center_IP}:${Registry_Center_Port}/eureka/"
