ab8f70162450c94d22807a18a0e2b9a9c7250414
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-19 11:50:04 +0800

    提交

--- cbb_parent/cbb_register_center/deploy/jenkins_build.sh
+++ cbb_parent/cbb_register_center/deploy/jenkins_build.sh
@@ -1,25 +1,25 @@
 #!/bin/sh -l
 #项目名称
-projectName="cbb_register_center"
-
-source ${WORKSPACE}/cbb_parent/env/env.sh
-
-sed -i 's,${projectName},'${projectName}',g' ./.env
-
-sed -i 's,${WebServer_Port},'${Registry_Center_Port}',g' ./.env
-
-sed -i 's,${spring_boot_admin_username},'${spring_boot_admin_username}',g' ./.env
-sed -i 's,${spring_boot_admin_password},'${spring_boot_admin_password}',g' ./.env
-
-
-echo "编译cbb_register_center"
-
-cd ${WORKSPACE}/cbb_parent
-
-mvn clean
-
-mvn install
-
+#projectName="cbb_register_center"
+#
+#source ${WORKSPACE}/cbb_parent/env/env.sh
+#
+#sed -i 's,${projectName},'${projectName}',g' ./.env
+#
+#sed -i 's,${WebServer_Port},'${Registry_Center_Port}',g' ./.env
+#
+#sed -i 's,${spring_boot_admin_username},'${spring_boot_admin_username}',g' ./.env
+#sed -i 's,${spring_boot_admin_password},'${spring_boot_admin_password}',g' ./.env
+#
+#
+#echo "编译cbb_register_center"
+#
+#cd ${WORKSPACE}/cbb_parent
+#
+#mvn clean
+#
+#mvn install
+#
 
 cd ${WORKSPACE}/cbb_parent/${projectName}/deploy
 
@@ -30,17 +30,17 @@ docker-compose -p ${projectName} down  --rmi all
 docker-compose -p ${projectName} up  -d
 
 
-echo "打包cbb_register_center(Windows版本)"
-
-if [ ! -d ${fileStoragePath}/${projectName} ]
-then
-  echo "装载java后台启动程序包装模板"
-  cp -rf ${java_service_wrapper_template_path} ${fileStoragePath}/${projectName}
-  sed -i 's,${AppName},'${projectName}',g' ${fileStoragePath}/${projectName}/conf/wrapper.conf
-fi
-
-cd ${WORKSPACE}/cbb_parent/${projectName}/backend/target
-
-cp -rf backend.jar ${fileStoragePath}/${projectName}/bin
-cp -rf lib ${fileStoragePath}/${projectName}/bin/lib
-cp -rf config ${fileStoragePath}/${projectName}/bin/config
+#echo "打包cbb_register_center(Windows版本)"
+#
+#if [ ! -d ${fileStoragePath}/${projectName} ]
+#then
+#  echo "装载java后台启动程序包装模板"
+#  cp -rf ${java_service_wrapper_template_path} ${fileStoragePath}/${projectName}
+#  sed -i 's,${AppName},'${projectName}',g' ${fileStoragePath}/${projectName}/conf/wrapper.conf
+#fi
+#
+#cd ${WORKSPACE}/cbb_parent/${projectName}/backend/target
+#
+#cp -rf backend.jar ${fileStoragePath}/${projectName}/bin
+#cp -rf lib ${fileStoragePath}/${projectName}/bin/lib
+#cp -rf config ${fileStoragePath}/${projectName}/bin/config
