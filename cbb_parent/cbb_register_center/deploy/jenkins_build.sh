c326375c19ba19afd97c39fd92e1a78537567de4
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-19 13:15:27 +0800

    提交

--- cbb_parent/cbb_register_center/deploy/jenkins_build.sh
+++ cbb_parent/cbb_register_center/deploy/jenkins_build.sh
@@ -16,9 +16,9 @@ echo "编译cbb_register_center"
 
 cd ${WORKSPACE}/cbb_parent
 
-#mvn clean
-#
-#mvn install
+mvn clean
+
+mvn install
 
 
 cd ${WORKSPACE}/cbb_parent/${projectName}/deploy
@@ -31,17 +31,17 @@ docker-compose -p ${projectName} down  --rmi all
 docker-compose -p ${projectName} up  -d
 
 
-#echo "打包cbb_register_center(Windows版本)"
-#
-#if [ ! -d ${fileStoragePath}/${projectName} ]
-#then
-#  echo "装载java后台启动程序包装模板"
-#  cp -rf ${java_service_wrapper_template_path} ${fileStoragePath}/${projectName}
-#  sed -i 's,${AppName},'${projectName}',g' ${fileStoragePath}/${projectName}/conf/wrapper.conf
-#fi
-#
-#cd ${WORKSPACE}/cbb_parent/${projectName}/backend/target
-#
-#cp -rf backend.jar ${fileStoragePath}/${projectName}/bin
-#cp -rf lib ${fileStoragePath}/${projectName}/bin/lib
-#cp -rf config ${fileStoragePath}/${projectName}/bin/config
+echo "打包cbb_register_center(Windows版本)"
+
+if [ ! -d ${fileStoragePath}/${projectName} ]
+then
+  echo "装载java后台启动程序包装模板"
+  cp -rf ${java_service_wrapper_template_path} ${fileStoragePath}/${projectName}
+  sed -i 's,${AppName},'${projectName}',g' ${fileStoragePath}/${projectName}/conf/wrapper.conf
+fi
+
+cd ${WORKSPACE}/cbb_parent/${projectName}/backend/target
+
+cp -rf backend.jar ${fileStoragePath}/${projectName}/bin
+cp -rf lib ${fileStoragePath}/${projectName}/bin/lib
+cp -rf config ${fileStoragePath}/${projectName}/bin/config
