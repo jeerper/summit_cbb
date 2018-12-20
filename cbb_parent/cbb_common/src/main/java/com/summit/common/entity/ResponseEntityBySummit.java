7bdc11e552b648259018d65956326b5a5175dbd1
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-20 09:37:11 +0800

    提交

--- cbb_parent/cbb_common/src/main/java/com/summit/common/entity/ResponseEntityBySummit.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/entity/ResponseEntityBySummit.java
@@ -89,5 +89,27 @@ public class ResponseEntityBySummit<T> implements Serializable {
         this.code = httpStatus.value();
     }
 
+    public int getCode() {
+        return code;
+    }
 
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 }
