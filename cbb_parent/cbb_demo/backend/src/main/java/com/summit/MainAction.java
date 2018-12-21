f36a0f25c67c62f48e3b8d94076108a1a3b78132
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-21 16:31:29 +0800

    更改demo

--- cbb_parent/cbb_demo/backend/src/main/java/com/summit/MainAction.java
+++ cbb_parent/cbb_demo/backend/src/main/java/com/summit/MainAction.java
@@ -3,21 +3,12 @@ package com.summit;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        JpaRepositoriesAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class}
-)
+@SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.summit.dao")
 public class MainAction {
