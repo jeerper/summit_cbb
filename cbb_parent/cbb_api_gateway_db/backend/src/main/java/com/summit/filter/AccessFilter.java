3a95cd9a4f0e40ef8af2eedd097a39a016c4e50f
liuyuan <405653510@qq.com>
2018-12-23 16:49:15 +0800

    增加过滤器

---
+++ cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/filter/AccessFilter.java
@@ -0,0 +1,43 @@
+package com.summit.filter;
+
+import cn.hutool.core.collection.CollectionUtil;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.summit.common.constant.CommonConstant;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.FORM_BODY_WRAPPER_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            requestContext.addZuulRequestHeader(CommonConstant.USER_HEADER, authentication.getName());
+            requestContext.addZuulRequestHeader(CommonConstant.ROLE_HEADER, CollectionUtil.join(authentication.getAuthorities(), ","));
+        }
+        return null;
+    }
+}
