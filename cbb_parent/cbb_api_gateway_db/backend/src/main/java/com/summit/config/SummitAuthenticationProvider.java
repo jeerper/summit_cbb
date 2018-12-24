2bfe8b1db32ef6c5785668766e3fbee7a6e4ebea
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-24 10:33:13 +0800

    commit

--- cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/config/SummitAuthenticationProvider.java
+++ cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/config/SummitAuthenticationProvider.java
@@ -1,19 +1,32 @@
 package com.summit.config;
 
+import cn.hutool.core.codec.Base64;
+import cn.hutool.core.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 public class SummitAuthenticationProvider extends DaoAuthenticationProvider {
+    private static  final Logger LOGGER= LoggerFactory.getLogger(SummitAuthenticationProvider.class);
+
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/CBC/NOPadding";
+    @Value("${password.encode.key}")
+    private String key;
+
     @Override
-    public void additionalAuthenticationChecks(UserDetails userDetails,
-                                               UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
-
-
+    public  void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
         if (authentication.getCredentials() == null) {
-            logger.debug("Authentication failed: no credentials provided");
+            LOGGER.debug("Authentication failed: no credentials provided");
 
             throw new BadCredentialsException(messages.getMessage(
                     "AbstractUserDetailsAuthenticationProvider.badCredentials",
@@ -22,13 +35,28 @@ public class SummitAuthenticationProvider extends DaoAuthenticationProvider {
 
         String presentedPassword = authentication.getCredentials().toString();
 
+        try {
+            presentedPassword = decryptAES(presentedPassword, key);
+        } catch (Exception e) {
+            LOGGER.error("密码解密失败:{}", presentedPassword);
+        }
+
+
         if (!getPasswordEncoder().matches(presentedPassword, userDetails.getPassword())) {
-            logger.debug("Authentication failed: password does not match stored value");
+            LOGGER.debug("Authentication failed: password does not match stored value");
 
             throw new BadCredentialsException(messages.getMessage(
                     "AbstractUserDetailsAuthenticationProvider.badCredentials",
                     "Bad credentials"));
         }
+    }
+    private static String decryptAES(String data, String pass) throws Exception {
+        Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+        SecretKeySpec keyspec = new SecretKeySpec(pass.getBytes(), KEY_ALGORITHM);
+        IvParameterSpec ivspec = new IvParameterSpec(pass.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+        byte[] result = cipher.doFinal(Base64.decode(data.getBytes(CharsetUtil.UTF_8)));
+        return new String(result, CharsetUtil.UTF_8);
     }
 
 }
