7f56e32c7bc86bf9bf9d236ba9daeaaaeb92566a
summit_cloud2018/liuyuan <Ly15891193398>
2018-12-27 19:42:20 +0800

    commit

--- cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/config/UserDetailsServiceImpl.java
+++ cbb_parent/cbb_api_gateway_db/backend/src/main/java/com/summit/config/UserDetailsServiceImpl.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,31 +27,33 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
     @Autowired
-    private  RemoteUserAuthService remoteUserAuthService;
+    private RemoteUserAuthService remoteUserAuthService;
 
     /**
      * 通过用户名查找用户
+     *
      * @param username 用户名
      * @return
      * @throws UsernameNotFoundException
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        RestFulEntityBySummit<UserInfo> userInfoRestFulEntity= remoteUserAuthService.queryUserRoleByUserName(username);
+        RestFulEntityBySummit<UserInfo> userInfoRestFulEntity = remoteUserAuthService.queryUserRoleByUserName(username);
 
-        //TODO:来自用户服务组件
-        UserBean user = new UserBean();
-        user.setUserName("admin");
-        user.setPassword("$2a$10$qXkUoWj.gogGI49w1f/UXeIDVvviYhM0wmZD3uAN3CqmJsQf5unNK");
-        user.setName("系统管理员");
-        user.setIsEnabled(1);
-        user.setRoles(new String[]{"ROLE_ADMIN"});
-        if (user == null) {
+        if (userInfoRestFulEntity.getCode() != 200) {
             throw new UsernameNotFoundException("用户不存在");
         }
+        UserInfo userInfo=userInfoRestFulEntity.getData();
+
+        UserBean user = new UserBean();
+        user.setUserName(userInfo.getUserName());
+        user.setPassword(userInfo.getPassword());
+        user.setName(userInfo.getName());
+        user.setIsEnabled(userInfo.getIsEnabled());
+        user.setRoles(userInfo.getRoles());
 
         List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(user.getRoles());
-        authorities.add(new SimpleGrantedAuthority("ROLE_DEFAULT"));
+//        authorities.add(new SimpleGrantedAuthority("ROLE_DEFAULT"));
         user.setAuthorities(authorities);
         return user;
     }
