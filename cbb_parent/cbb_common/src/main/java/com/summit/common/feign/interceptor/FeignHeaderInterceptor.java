8d373e671be4bafba9877c1981aa4c1ccfd01cce
summit_cloud2018/liuyuan <Ly15891193398>
2019-01-23 20:06:52 +0800

    commit

---
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/feign/interceptor/FeignHeaderInterceptor.java
@@ -0,0 +1,30 @@
+package com.summit.common.feign.interceptor;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * feign请求头部信息追加拦截器
+ * @author Administrator
+ */
+public class FeignHeaderInterceptor implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                String values = request.getHeader(name);
+                requestTemplate.header(name, values);
+            }
+        }
+    }
+}
