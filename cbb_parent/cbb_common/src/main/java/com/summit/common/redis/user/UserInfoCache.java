3e697dd67f47f40425a298b617c60c70c83fa777
summit_cloud2018/liuyuan <Ly15891193398>
2019-01-25 16:24:10 +0800

    commit

--- cbb_parent/cbb_common/src/main/java/com/summit/common/redis/user/UserInfoCache.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/redis/user/UserInfoCache.java
@@ -34,7 +34,8 @@ public class UserInfoCache {
         try {
             String userInfoString = objectMapper.writeValueAsString(userInfo);
             LOGGER.debug("设置用户信息缓存:{}:{}", userName, userInfoString);
-            redisTemplateWithUserInfo.opsForValue().set(CommonConstant.USER_INFO_CACHE_PREFIX + userName, userInfoString);
+            redisTemplateWithUserInfo.opsForValue().set(CommonConstant.USER_INFO_CACHE_PREFIX + userName,
+                    userInfoString);
         } catch (Exception e) {
             LOGGER.error("设置用户信息缓存失败", e);
         }
@@ -48,9 +49,10 @@ public class UserInfoCache {
      */
     public UserInfo getUserInfo(String userName) {
         try {
-            String userInfoString = redisTemplateWithUserInfo.opsForValue().get(CommonConstant.USER_INFO_CACHE_PREFIX + userName);
+            String userInfoString =
+                    redisTemplateWithUserInfo.opsForValue().get(CommonConstant.USER_INFO_CACHE_PREFIX + userName);
             LOGGER.debug("读取用户信息缓存:{}:{}", userName, userInfoString);
-            if(StrUtil.isBlank(userInfoString)){
+            if (StrUtil.isBlank(userInfoString)) {
                 return null;
             }
             return objectMapper.readValue(userInfoString, UserInfo.class);
@@ -58,5 +60,16 @@ public class UserInfoCache {
             LOGGER.error("获取用户信息缓存失败", e);
             return null;
         }
+    }
+
+    /**
+     * 删除用户信息缓存
+     * @param userName 用户名称
+     * @return
+     */
+    public boolean deleteUserInfo(String userName) {
+        boolean result = redisTemplateWithUserInfo.delete(CommonConstant.USER_INFO_CACHE_PREFIX + userName);
+        LOGGER.debug("删除用户信息缓存:{}:{}", userName, result);
+        return true;
     }
 }
