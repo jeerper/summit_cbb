dcc83dd981bf8cd05cde3381d0f389ede7a6448c
summit_cloud2018/liuyuan <Ly15891193398>
2019-03-15 16:58:17 +0800

    commit

--- cbb_parent/cbb_api_gateway_db/src/main/resources/db/V1.2__data.sql
+++ cbb_parent/cbb_api_gateway_db/src/main/resources/db/V1.2__data.sql
@@ -1,1 +1,1 @@
-INSERT INTO `test` VALUES ('1', '测试');
+INSERT INTO `oauth_client_details` VALUES ('summit', null, '$2a$10$MkTZ3tYIaUy5tRuBl1/QJuasFwpkeWAKoiRacLQhbJDzgZ1a5wQwy', 'server', 'password,refresh_token', null, null, null, null, null, 'true');
