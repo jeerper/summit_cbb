18a3501b182121f812c21ac11839a0afd8cfa1f5
summit_cloud2018/liuyuan <Ly15891193398>
2019-03-21 19:11:44 +0800

    网关路由持久化配置

--- cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/DynamicRouteLocator.java
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/DynamicRouteLocator.java
@@ -1,5 +1,9 @@
 package com.summit.config;
 
+import cn.hutool.core.util.StrUtil;
+import com.summit.dao.entity.GatewayRouteInfo;
+import com.summit.dao.repository.GatewayRouteInfoDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -8,89 +12,81 @@ import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.discovery.DiscoveryClientRouteLocator;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @Component
 public class DynamicRouteLocator extends DiscoveryClientRouteLocator {
 
+    @Autowired
+    ZuulProperties properties;
+    @Autowired
+    GatewayRouteInfoDao gatewayRouteInfoDao;
 
     @Autowired
-    public DynamicRouteLocator(DispatcherServletPath dispatcherServletPath, DiscoveryClient discovery, ZuulProperties zuulProperties,
+    public DynamicRouteLocator(DispatcherServletPath dispatcherServletPath, DiscoveryClient discovery,
+                               ZuulProperties zuulProperties,
                                Registration registration) {
         super(dispatcherServletPath.getPrefix(), discovery, zuulProperties, registration);
     }
 
-//    /**
-//     * 重写路由配置
-//     * <p>
-//     * 1. properties 配置。
-//     * 2. eureka 默认配置。
-//     * 3. DB数据库配置。
-//     *
-//     * @return 路由表
-//     */
-//    @Override
-//    protected LinkedHashMap<String, ZuulProperties.ZuulRoute> locateRoutes() {
-//        LinkedHashMap<String, ZuulProperties.ZuulRoute> routesMap = new LinkedHashMap<>();
-//        //读取properties配置、eureka默认配置
-//        routesMap.putAll(super.locateRoutes());
-//        log.debug("初始默认的路由配置完成");
-//        routesMap.putAll(locateRoutesFromDb());
-//        LinkedHashMap<String, ZuulProperties.ZuulRoute> values = new LinkedHashMap<>();
-//        for (Map.Entry<String, ZuulProperties.ZuulRoute> entry : routesMap.entrySet()) {
-//            String path = entry.getKey();
-//            if (!path.startsWith("/")) {
-//                path = "/" + path;
-//            }
-//            if (StrUtil.isNotBlank(this.properties.getPrefix())) {
-//                path = this.properties.getPrefix() + path;
-//                if (!path.startsWith("/")) {
-//                    path = "/" + path;
-//                }
-//            }
-//            values.put(path, entry.getValue());
-//        }
-//        return values;
-//    }
-//
-//    /**
-//     * Redis中保存的，没有从upms拉去，避免启动链路依赖问题（取舍），网关依赖业务模块的问题
-//     *
-//     * @return
-//     */
-//    private Map<String, ZuulProperties.ZuulRoute> locateRoutesFromDb() {
-//        Map<String, ZuulProperties.ZuulRoute> routes = new LinkedHashMap<>();
-//
-//        Object obj = redisTemplate.opsForValue().get(CommonConstant.ROUTE_KEY);
-//        if (obj == null) {
-//            return routes;
-//        }
-//
-//        List<SysZuulRoute> results = (List<SysZuulRoute>) obj;
-//        for (SysZuulRoute result : results) {
-//            if (StrUtil.isBlank(result.getPath()) && StrUtil.isBlank(result.getUrl())) {
-//                continue;
-//            }
-//
-//            ZuulProperties.ZuulRoute zuulRoute = new ZuulProperties.ZuulRoute();
-//            try {
-//                zuulRoute.setId(result.getServiceId());
-//                zuulRoute.setPath(result.getPath());
-//                zuulRoute.setServiceId(result.getServiceId());
-//                zuulRoute.setRetryable(StrUtil.equals(result.getRetryable(), "0") ? Boolean.FALSE : Boolean.TRUE);
-//                zuulRoute.setStripPrefix(StrUtil.equals(result.getStripPrefix(), "0") ? Boolean.FALSE : Boolean.TRUE);
-//                zuulRoute.setUrl(result.getUrl());
-//                List<String> sensitiveHeadersList = StrUtil.splitTrim(result.getSensitiveheadersList(), ",");
-//                if (sensitiveHeadersList != null) {
-//                    Set<String> sensitiveHeaderSet = CollUtil.newHashSet();
-//                    sensitiveHeadersList.forEach(sensitiveHeader -> sensitiveHeaderSet.add(sensitiveHeader));
-//                    zuulRoute.setSensitiveHeaders(sensitiveHeaderSet);
-//                    zuulRoute.setCustomSensitiveHeaders(true);
-//                }
-//            } catch (Exception e) {
-//                log.error("从数据库加载路由配置异常", e);
-//            }
-//            log.debug("添加数据库自定义的路由配置,path：{}，serviceId:{}", zuulRoute.getPath(), zuulRoute.getServiceId());
-//            routes.put(zuulRoute.getPath(), zuulRoute);
-//        }
-//        return routes;
-//    }
+    /**
+     * 加载路由配置
+     *
+     * @return 路由表
+     */
+    @Override
+    protected LinkedHashMap<String, ZuulProperties.ZuulRoute> locateRoutes() {
+        LinkedHashMap<String, ZuulProperties.ZuulRoute> routesMap = new LinkedHashMap<>();
+        //读取properties配置、eureka默认配置
+        log.debug("加载配置文件和注册中心中的路由信息");
+        routesMap.putAll(super.locateRoutes());
+        log.debug("加载数据库中的路由信息");
+        routesMap.putAll(locateRoutesFromDb());
+        LinkedHashMap<String, ZuulProperties.ZuulRoute> values = new LinkedHashMap<>();
+        for (Map.Entry<String, ZuulProperties.ZuulRoute> entry : routesMap.entrySet()) {
+            String path = entry.getKey();
+            if (!path.startsWith("/")) {
+                path = "/" + path;
+            }
+            if (StrUtil.isNotBlank(this.properties.getPrefix())) {
+                path = this.properties.getPrefix() + path;
+                if (!path.startsWith("/")) {
+                    path = "/" + path;
+                }
+            }
+            values.put(path, entry.getValue());
+        }
+        return values;
+    }
+
+
+    private Map<String, ZuulProperties.ZuulRoute> locateRoutesFromDb() {
+        Map<String, ZuulProperties.ZuulRoute> routes = new LinkedHashMap<>();
+        try {
+            List<GatewayRouteInfo> gatewayRouteInfoList = gatewayRouteInfoDao.selectList(null);
+            for (GatewayRouteInfo gatewayRouteInfo : gatewayRouteInfoList) {
+                if (StrUtil.isBlank(gatewayRouteInfo.getPath())) {
+                    continue;
+                }
+                if (gatewayRouteInfo.getEnabled() == 0) {
+                    continue;
+                }
+                ZuulProperties.ZuulRoute zuulRoute = new ZuulProperties.ZuulRoute();
+                zuulRoute.setId(gatewayRouteInfo.getServiceId());
+                zuulRoute.setPath(gatewayRouteInfo.getPath());
+                zuulRoute.setServiceId(gatewayRouteInfo.getServiceId());
+                zuulRoute.setStripPrefix(gatewayRouteInfo.getStripPrefix() == 1 ? Boolean.TRUE : Boolean.FALSE);
+                zuulRoute.setRetryable(gatewayRouteInfo.getRetryable() == 1 ? Boolean.TRUE : Boolean.FALSE);
+
+                log.debug("添加数据库的路由配置,serviceId：{}，path:{}", zuulRoute.getServiceId(), zuulRoute.getPath());
+                routes.put(zuulRoute.getPath(), zuulRoute);
+            }
+        } catch (Exception e) {
+            log.error("从数据库加载路由配置异常", e);
+        }
+        return routes;
+    }
 }
---
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/dao/repository/GatewayRouteInfoDao.java
@@ -0,0 +1,10 @@
+package com.summit.dao.repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.summit.dao.entity.GatewayRouteInfo;
+
+/**
+ * 网关数据Dao
+ */
+public interface GatewayRouteInfoDao extends BaseMapper<GatewayRouteInfo> {
+}
