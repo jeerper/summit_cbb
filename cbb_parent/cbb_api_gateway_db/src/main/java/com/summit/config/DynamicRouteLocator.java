05087796e6804adbae204ce0586a700930d78f09
summit_cloud2018/liuyuan <Ly15891193398>
2019-03-22 11:46:59 +0800

    网关路由动态注册开发

--- cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/DynamicRouteLocator.java
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/DynamicRouteLocator.java
@@ -12,6 +12,8 @@ import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.cloud.netflix.zuul.filters.discovery.DiscoveryClientRouteLocator;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -26,12 +28,14 @@ public class DynamicRouteLocator extends DiscoveryClientRouteLocator {
     ZuulProperties properties;
     @Autowired
     GatewayRouteInfoDao gatewayRouteInfoDao;
+    private DiscoveryClient discovery;
 
     @Autowired
     public DynamicRouteLocator(DispatcherServletPath dispatcherServletPath, DiscoveryClient discovery,
                                ZuulProperties zuulProperties,
                                Registration registration) {
         super(dispatcherServletPath.getPrefix(), discovery, zuulProperties, registration);
+        this.discovery = discovery;
     }
 
     /**
@@ -41,12 +45,21 @@ public class DynamicRouteLocator extends DiscoveryClientRouteLocator {
      */
     @Override
     protected LinkedHashMap<String, ZuulProperties.ZuulRoute> locateRoutes() {
+
         LinkedHashMap<String, ZuulProperties.ZuulRoute> routesMap = new LinkedHashMap<>();
-        //读取properties配置、eureka默认配置
-        log.debug("加载配置文件和注册中心中的路由信息");
-        routesMap.putAll(super.locateRoutes());
+
+        //读取properties中的路由配置
+        log.debug("加载配置文件中的路由信息");
+        for (ZuulProperties.ZuulRoute route : this.properties.getRoutes().values()) {
+            routesMap.put(route.getPath(), route);
+        }
+
+        log.debug("加载注册中心中的路由信息");
+        routesMap.putAll(locateRoutesFromRegisterCenter());
+
         log.debug("加载数据库中的路由信息");
         routesMap.putAll(locateRoutesFromDb());
+
         LinkedHashMap<String, ZuulProperties.ZuulRoute> values = new LinkedHashMap<>();
         for (Map.Entry<String, ZuulProperties.ZuulRoute> entry : routesMap.entrySet()) {
             String path = entry.getKey();
@@ -64,7 +77,64 @@ public class DynamicRouteLocator extends DiscoveryClientRouteLocator {
         return values;
     }
 
+    /**
+     * 加载注册中心中的路由信息
+     *
+     * @return
+     */
+    private Map<String, ZuulProperties.ZuulRoute> locateRoutesFromRegisterCenter() {
+        Map<String, ZuulProperties.ZuulRoute> routesMap = new LinkedHashMap<>();
+        if (this.discovery != null) {
+            Map<String, ZuulProperties.ZuulRoute> staticServices = new LinkedHashMap<>();
+            for (ZuulProperties.ZuulRoute route : routesMap.values()) {
+                String serviceId = route.getServiceId();
+                if (serviceId == null) {
+                    serviceId = route.getId();
+                }
+                if (serviceId != null) {
+                    staticServices.put(serviceId, route);
+                }
+            }
+            // Add routes for discovery services by default
+            List<String> services = this.discovery.getServices();
+            String[] ignored = this.properties.getIgnoredServices()
+                    .toArray(new String[0]);
+            for (String serviceId : services) {
+                // Ignore specifically ignored services and those that were manually
+                // configured
+                String key = "/" + mapRouteToService(serviceId) + "/**";
+                if (staticServices.containsKey(serviceId)
+                        && staticServices.get(serviceId).getUrl() == null) {
+                    // Explicitly configured with no URL, cannot be ignored
+                    // all static routes are already in routesMap
+                    // Update location using serviceId if location is null
+                    ZuulProperties.ZuulRoute staticRoute = staticServices.get(serviceId);
+                    if (!StringUtils.hasText(staticRoute.getLocation())) {
+                        staticRoute.setLocation(serviceId);
+                    }
+                }
+                if (!PatternMatchUtils.simpleMatch(ignored, serviceId)
+                        && !routesMap.containsKey(key)) {
+                    // Not ignored
+                    routesMap.put(key, new ZuulProperties.ZuulRoute(key, serviceId));
+                }
+            }
+        }
+        if (routesMap.get(DEFAULT_ROUTE) != null) {
+            ZuulProperties.ZuulRoute defaultRoute = routesMap.get(DEFAULT_ROUTE);
+            // Move the defaultServiceId to the end
+            routesMap.remove(DEFAULT_ROUTE);
+            routesMap.put(DEFAULT_ROUTE, defaultRoute);
+        }
+        return routesMap;
 
+    }
+
+    /**
+     * 加载数据库中的路由信息
+     *
+     * @return
+     */
     private Map<String, ZuulProperties.ZuulRoute> locateRoutesFromDb() {
         Map<String, ZuulProperties.ZuulRoute> routes = new LinkedHashMap<>();
         try {
@@ -90,5 +160,11 @@ public class DynamicRouteLocator extends DiscoveryClientRouteLocator {
             log.error("从数据库加载路由配置异常", e);
         }
         return routes;
+    }
+
+    @Override
+    public void refresh() {
+        doRefresh();
+        log.debug("刷新");
     }
 }
