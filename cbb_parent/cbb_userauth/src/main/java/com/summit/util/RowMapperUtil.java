deb1d64f5b67e5f8c3036a4bb49c62b450d1763e
liuyonghong <liuyonghong_lyh@163.com>
2019-04-02 16:28:48 +0800

    数据转换工具类

---
+++ cbb_parent/cbb_userauth/src/main/java/com/summit/util/RowMapperUtil.java
@@ -0,0 +1,56 @@
+package com.summit.util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RowMapperUtil {
+	public String resultSetGetString(ResultSet rs, String columnLabel) {
+		try {
+			if (rs.getObject(columnLabel) == null) {
+				return null;
+			}
+			return rs.getString(columnLabel);
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		}
+	}
+	public Integer resultSetGetInt(ResultSet rs, String columnLabel) {
+		try {
+			if (rs.getObject(columnLabel) == null) {
+				return null;
+			}
+			return rs.getInt(columnLabel);
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		}
+	}
+	public Double resultSetGetDouble(ResultSet rs, String columnLabel) {
+		try {
+			if (rs.getObject(columnLabel) == null) {
+				return null;
+			}
+			return rs.getDouble(columnLabel);
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		}
+	}
+	public Timestamp resultSetGetTimestamp(ResultSet rs, String columnLabel) {
+		try {
+			if (rs.getObject(columnLabel) == null) {
+				return null;
+			}
+			return rs.getTimestamp(columnLabel);
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			return null;
+		}
+	}
+	
+}
