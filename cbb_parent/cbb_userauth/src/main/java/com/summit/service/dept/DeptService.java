93bfa5de04d55cdaa7e3fec4b716578113de3966
liuyonghong <liuyonghong_lyh@163.com>
2019-04-15 11:27:31 +0800

    部门分页查询方法中添加上级部门展示

--- cbb_parent/cbb_userauth/src/main/java/com/summit/service/dept/DeptService.java
+++ cbb_parent/cbb_userauth/src/main/java/com/summit/service/dept/DeptService.java
@@ -144,7 +144,7 @@ public class DeptService {
 	 * @throws SQLException 
 	 */
 	public Page<DeptBean> queryByPage(int start, int limit, JSONObject paramJson) throws SQLException {
-		StringBuffer sql = new StringBuffer("SELECT dept.*, fdept.DEPTNAME as pdeptName FROM SYS_DEPT dept left join SYS_DEPT fdept on dept.pid=fdept.DEPTCODE where 1=1 ");
+		StringBuffer sql = new StringBuffer("SELECT dept.*, fdept.DEPTNAME as pdeptName FROM SYS_DEPT dept left join SYS_DEPT fdept on dept.pid=fdept.id where 1=1 ");
 		if(paramJson!=null && !paramJson.isEmpty()){
         	if(paramJson.containsKey("pid")    ){
         		sql.append(" and dept.pid = "+paramJson.get("pid")+" ");
