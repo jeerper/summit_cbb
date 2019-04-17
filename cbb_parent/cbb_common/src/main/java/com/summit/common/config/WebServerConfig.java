e36817ced22462d37217f146e04dc16f3b96c590
summit_cloud2018/liuyuan <Ly15891193398>
2019-04-17 15:22:39 +0800

    commit

--- cbb_parent/cbb_common/src/main/java/com/summit/common/config/WebServerConfig.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/config/WebServerConfig.java
@@ -1,10 +1,13 @@
 package com.summit.common.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 public class WebServerConfig {
@@ -21,5 +24,20 @@ public class WebServerConfig {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大(KB,MB)
+        factory.setMaxFileSize("1024MB");
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("10240MB");
+        return factory.createMultipartConfig();
+    }
+
 
 }
