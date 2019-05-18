576b884565d6768243982be461aee541aecc226b
liuyonghong <liuyonghong_lyh@163.com>
2019-05-18 13:36:07 +0800

    添加分页类

---
+++ cbb_parent/cbb_utils/src/main/java/com/summit/cbb/utils/page/Page.java
@@ -0,0 +1,38 @@
+package com.summit.cbb.utils.page;
+
+import java.util.List;
+
+
+
+public class Page<T> {
+
+	private List<T> content;
+	private Pageable pageable;
+	
+	public Page() {
+		super();
+	}
+
+	public Page(List<T> content, Pageable pageable) {
+		super();
+		this.content = content;
+		this.pageable = pageable;
+	}
+
+	public List<T> getContent() {
+		return content;
+	}
+
+	public void setContent(List<T> content) {
+		this.content = content;
+	}
+
+	public Pageable getPageable() {
+		return pageable;
+	}
+
+	public void setPageable(Pageable pageable) {
+		this.pageable = pageable;
+	}
+	
+}
