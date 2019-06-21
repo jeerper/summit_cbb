60178c494d89e72ffa5762213e730666da64aefc
LiuYuan <ly405653510@gmail.com>
2019-06-21 14:31:32 +0800

    commit

---
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/config/FeignMultipartSupportConfig.java
@@ -0,0 +1,25 @@
+package com.summit.common.config;
+
+import com.summit.common.feign.form.SpringMultipartEncoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class FeignMultipartSupportConfig {
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+    @Bean
+    @Primary
+    @Scope("prototype")
+    public Encoder feignEncoder() {
+        return new SpringMultipartEncoder(new SpringEncoder(messageConverters));
+    }
+}
---
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/feign/form/SpringMultipartEncoder.java
@@ -0,0 +1,81 @@
+package com.summit.common.feign.form;
+
+import feign.RequestTemplate;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
+import feign.form.FormEncoder;
+import feign.form.MultipartFormContentProcessor;
+import feign.form.spring.SpringManyMultipartFilesWriter;
+import feign.form.spring.SpringSingleMultipartFileWriter;
+import lombok.val;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import static feign.form.ContentType.MULTIPART;
+import static java.util.Collections.singletonMap;
+
+/**
+ * Adds support for {@link MultipartFile} type to {@link FormEncoder}.
+ *
+ * @author Tomasz Juchniewicz &lt;tjuchniewicz@gmail.com&gt;
+ * @since 14.09.2016
+ */
+public class SpringMultipartEncoder extends FormEncoder {
+
+  /**
+   * Constructor with the default Feign's encoder as a delegate.
+   */
+  public SpringMultipartEncoder() {
+    this(new Encoder.Default());
+  }
+
+  /**
+   * Constructor with specified delegate encoder.
+   *
+   * @param delegate  delegate encoder, if this encoder couldn't encode object.
+   */
+  public SpringMultipartEncoder(Encoder delegate) {
+    super(delegate);
+
+    val processor = (MultipartFormContentProcessor) getContentProcessor(MULTIPART);
+    processor.addWriter(new SpringSingleMultipartFileWriter());
+    processor.addWriter(new SpringManyMultipartFilesWriter());
+  }
+
+  @Override
+  public void encode (Object object, Type bodyType, RequestTemplate template) throws EncodeException {
+    if (bodyType.equals(MultipartFile[].class)) {
+      val files = (MultipartFile[]) object;
+      val data = new HashMap<String, Object>(files.length, 1.F);
+      for (val file : files) {
+        data.put(file.getName(), file);
+      }
+      super.encode(data, MAP_STRING_WILDCARD, template);
+    } else if (bodyType.equals(MultipartFile.class)) {
+      val file = (MultipartFile) object;
+      val data = singletonMap(file.getName(), object);
+      super.encode(data, MAP_STRING_WILDCARD, template);
+    } else if (isMultipartFileCollection(object)) {
+      val iterable = (Iterable<?>) object;
+      val data = new HashMap<String, Object>();
+      for (val item : iterable) {
+        val file = (MultipartFile) item;
+        data.put(file.getName(), file);
+      }
+      super.encode(data, MAP_STRING_WILDCARD, template);
+    } else {
+      super.encode(object, bodyType, template);
+    }
+  }
+
+  private boolean isMultipartFileCollection (Object object) {
+    if (!(object instanceof Iterable)) {
+      return false;
+    }
+    val iterable = (Iterable<?>) object;
+    val iterator = iterable.iterator();
+    return iterator.hasNext() && iterator.next() instanceof MultipartFile;
+  }
+}
