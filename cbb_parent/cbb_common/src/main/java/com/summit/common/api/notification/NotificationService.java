9069a32f028858f798b9cdc7864de9fec7eb89b1
LiuYuan <ly405653510@gmail.com>
2019-06-24 18:07:25 +0800

    commit

--- cbb_parent/cbb_common/src/main/java/com/summit/common/api/notification/NotificationService.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/api/notification/NotificationService.java
@@ -2,15 +2,7 @@ package com.summit.common.api.notification;
 
 
 import com.summit.common.config.FeignMultipartSupportConfig;
-import com.summit.common.entity.RestfulEntityBySummit;
-import com.summit.common.entity.notification.EmailInfo;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * 消息通知组件远程通信接口
@@ -18,9 +10,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @FeignClient(value = "cbb-notification",configuration = FeignMultipartSupportConfig.class)
 public interface NotificationService {
 
-
-    @RequestMapping(value = "/msg/email",method = POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    RestfulEntityBySummit<String> sendMail(EmailInfo emailInfo, @RequestPart MultipartFile[] attachMultipartFiles, @RequestPart MultipartFile[] imagesMultipartFiles);
 
 //    @PostMapping("/msg/email2")
 //    RestfulEntityBySummit<String> sendMail(@RequestPart MultipartFile[] attachMultipartFiles);
--- cbb_parent/cbb_weather/src/main/java/com/summit/weather/controller/TestController.java
+++ cbb_parent/cbb_weather/src/main/java/com/summit/weather/controller/TestController.java
@@ -1,13 +1,9 @@
 package com.summit.weather.controller;
 
 import com.summit.common.api.notification.NotificationService;
-import com.summit.common.entity.RestfulEntityBySummit;
-import com.summit.common.entity.notification.EmailInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/msg")
@@ -15,11 +11,6 @@ public class TestController {
     @Autowired
     private NotificationService notificationService;
 
-    @PostMapping("/email")
-    public RestfulEntityBySummit sendEmail(EmailInfo emailInfo, MultipartFile[] attachMultipartFiles, MultipartFile[] imagesMultipartFiles){
-        RestfulEntityBySummit result = notificationService.sendMail(emailInfo,attachMultipartFiles,imagesMultipartFiles);
 
-        return result;
-    }
 
 }
