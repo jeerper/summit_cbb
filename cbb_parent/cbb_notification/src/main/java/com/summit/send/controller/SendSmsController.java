6a98f7fade9713284de853d3f1b48b232c8b010f
wangyx <zhanganchu1@qq.com>
2019-06-25 18:09:45 +0800

    发送短信服务controller

--- cbb_parent/cbb_notification/src/main/java/com/summit/send/controller/SendSmsController.java
+++ cbb_parent/cbb_notification/src/main/java/com/summit/send/controller/SendSmsController.java
@@ -1,7 +1,13 @@
 package com.summit.send.controller;
 
-import com.summit.send.pojo.SendSms;
+import com.summit.common.entity.ResponseCodeEnum;
+import com.summit.common.entity.RestfulEntityBySummit;
+import com.summit.common.entity.notification.SendSms;
+import com.summit.common.util.ResultBuilder;
+import com.summit.send.service.SendSmsService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/msg")
 public class SendSmsController {
 
+    @Autowired
+    private SendSmsService sendSmsService;
+
+    @ApiOperation(value = "发送短信",  notes = "接收者号码(phoneNumbers)，签名(signNames),模板号(templateCode)都是必输项,其中phoneNumbers和signNames可传多个，且一一对应")
     @PostMapping("/sms")
-    public SendSms sendSms(@RequestBody SendSms sendSms){
+    public RestfulEntityBySummit sendSms(@RequestBody SendSms sendSms){
         log.info("==========");
         System.out.println(sendSms);
-        return sendSms;
+        return sendSmsService.sendSms(sendSms);
     }
 }
