244b3c8514fc25b30e7e7353e61b2fc6183fd7ca
wangyx <zhanganchu1@qq.com>
2019-06-25 17:56:50 +0800

    提供消息发送fegin接口

--- cbb_parent/cbb_common/src/main/java/com/summit/common/api/notification/NotificationService.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/api/notification/NotificationService.java
@@ -1,17 +1,81 @@
 package com.summit.common.api.notification;
 
-
-import com.summit.common.config.FeignMultipartSupportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
+import com.summit.common.entity.RestfulEntityBySummit;
+import com.summit.common.entity.notification.MyIntFile;
+import com.summit.common.entity.notification.SendSms;
+import feign.Param;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 /**
  * 消息通知组件远程通信接口
  */
-@FeignClient(value = "cbb-notification",configuration = FeignMultipartSupportConfig.class)
+@FeignClient(value = "cbb-notification"/*,configuration = FeignMultipartSupportConfig.class*/)
 public interface NotificationService {
 
+    @RequestMapping(value = "/msg/sms",method = RequestMethod.POST)
+    RestfulEntityBySummit sendSms(@RequestBody SendSms sendSms);
 
 //    @PostMapping("/msg/email2")
+   /*@RequestMapping(value = "/msg/test",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit<String> sendMail(@RequestPart("emailInfo") EmailInfo emailInfo, @RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles, @RequestPart("imagesMultipartFiles") MultipartFile[] imagesMultipartFiles);*/
+
+
+    /*@RequestMapping(value = "/msg/email",method = POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit<String> sendMail(@RequestParam("attachMultipartFiles") MultipartFile[] attachMultipartFiles, @RequestParam("imagesMultipartFiles") MultipartFile[] imagesMultipartFiles);*/
+    /*@RequestMapping(value = "/msg/test2",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit sendMail(EmailInfo emailInfo, @RequestParam("attachMultipartFiles") MultipartFile[] attachMultipartFiles);*/
+    //     @RequestMapping(value = "/msg/emai3",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    //     RestfulEntityBySummit sendMail(EmailInfo emailInfo, @RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles);
+
+    @RequestMapping(value = "/msg/test3",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit sendMail(@RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles);
+    @RequestMapping(value = "/msg/test4",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit sendMail(@RequestParam("emailInfo") String emailInfo, @RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles);
+
+    @RequestMapping(value = "/msg/test5",method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    RestfulEntityBySummit sendMail5(@RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles);
+
+    @RequestMapping(value = "/msg/test6",method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    RestfulEntityBySummit sendMail(@RequestPart("attachMultipartFiles") MultipartFile[] attachMultipartFiles,@RequestParam("emailId") String emailId);
+
+    @RequestMapping(value = "/msg/test7",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(@Param("emailInfoAndAttachFiles") Map<String,  ?> emailInfoAndAttachFiles);
+
+    @RequestMapping(value = "/msg/test8",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail8(@Param("emailInfoAndAttachFiles") Map<String,  ?> emailInfoAndAttachFiles);
+
+    @RequestMapping(value = "/msg/test9",method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    @RequestLine("POST /msg/test8")
+    RestfulEntityBySummit sendMail(@RequestPart("fileObj") MultipartFile fileObj);
+
+    @RequestMapping(value = "/msg/test10",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(@RequestBody MyIntFile fileObj);
+
+    @RequestMapping(value = "/msg/test11",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(@RequestParam("aaa") int[] aaa);
+    @RequestMapping(value = "/msg/test12",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(@RequestParam("bytes") byte[] bytes);
+
+    @RequestMapping(value = "/msg/test13",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(/*@RequestBody */String[] str);
+
+    @RequestMapping(value = "/msg/test14",method = RequestMethod.POST)
+    RestfulEntityBySummit sendMail(@RequestParam("str") String str);
+
+
+//    @RequestMapping(value = "/msg/test6",method = RequestMethod.POST)
+//    RestfulEntityBySummit sendMail2(@Param("emailId") String emailId, @Param("attachMultipartFiles") MultipartFile attachMultipartFiles);
+
+    //    @PostMapping("/msg/email2")
 //    RestfulEntityBySummit<String> sendMail(@RequestPart MultipartFile[] attachMultipartFiles);
 //
 //    @PostMapping("/msg/email3")
