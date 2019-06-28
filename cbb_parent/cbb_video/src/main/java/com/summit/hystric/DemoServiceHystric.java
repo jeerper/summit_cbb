a48d537785ea714a65a1fedb85ddcd573d7148ec
LiuYuan <ly405653510@gmail.com>
2019-06-28 15:17:15 +0800

    增加视频组件工程

---
+++ cbb_parent/cbb_video/src/main/java/com/summit/hystric/DemoServiceHystric.java
@@ -0,0 +1,15 @@
+package com.summit.hystric;
+
+import com.summit.service.DemoService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoServiceHystric implements DemoService{
+
+	@Override
+	public String demo(String name) {
+		// TODO Auto-generated method stub
+		return "sorry" + name +" the request got error";
+	}
+
+}
---
+++ cbb_parent/cbb_video/src/main/java/com/summit/service/UserService.java
@@ -0,0 +1,29 @@
+package com.summit.service;
+
+import com.summit.dao.DemoDao;
+import com.summit.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    private DemoDao demoDao;
+
+
+
+    public List<User> getAllUser1() {
+        System.out.println(demoDao.selectCount(null));
+        return demoDao.selectList(null);
+    }
+
+    public List<User> getAllUser2() {
+        System.out.println(demoDao.selectCount(null));
+        return demoDao.selectList(null);
+    }
+
+}
