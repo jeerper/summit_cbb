db64ef8530686d6f14996962ef0daaee020d90f1
LiuYuan <ly405653510@gmail.com>
2019-06-28 15:33:35 +0800

    commit

--- cbb_parent/cbb_video/deploy/jenkins_build.sh
+++ cbb_parent/cbb_video/deploy/jenkins_build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -l
 set -e
 #项目名称
-projectName="cbb_demo"
+projectName="cbb_video"
 #组件名称
-component_name="共享组件脚手架"
+component_name="视频组件"
 #组件描述
-component_description="共享组件脚手架"
+component_description="视频组件"
 #组件标签
 component_tag=${projectName}
 
 source ${WORKSPACE}/cbb_parent/env/env.sh
 
-WebServer_Port=${Demo_Port}
+WebServer_Port=${Video_Port}
 
 source ${WORKSPACE}/cbb_parent/env/build_wrapper.sh
 
--- cbb_parent/cbb_video/src/main/java/com/summit/MainAction.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/MainAction.java
@@ -1,6 +1,5 @@
 package com.summit;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@MapperScan("com.summit.dao")
 public class MainAction {
 
 	public static void main(String[] args){
