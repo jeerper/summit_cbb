d8afe6952f844b52d5aeda5004465e7cac787a32
LiuYuan <ly405653510@gmail.com>
2019-07-08 15:10:50 +0800

    重构SDK

--- cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/api/HuaWeiSdkApi.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/api/HuaWeiSdkApi.java
@@ -1,11 +1,7 @@
 package com.summit.sdk.huawei.api;
 
-import cn.hutool.core.util.StrUtil;
 import com.summit.sdk.huawei.HWPuSDKLibrary;
-import com.summit.sdk.huawei.PU_DEVICE_REGISTER_RSP;
-import com.summit.sdk.huawei.PU_EVENT_COMMON;
-import com.summit.sdk.huawei.PU_EVENT_REGISTER;
-import com.summit.sdk.huawei.PU_SYSTEM_TIME;
+import com.summit.sdk.huawei.callback.EventInfoCallBack;
 import com.sun.jna.NativeLong;
 import com.sun.jna.ptr.NativeLongByReference;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +20,11 @@ public class HuaWeiSdkApi {
 
     private HWPuSDKLibrary.pfGetEventInfoCallBack pfGetEventInfoCallBack;
 
+    public static void printReturnMsg() {
+        NativeLong errorCode = HWPuSDKLibrary.INSTANCE.IVS_PU_GetLastError();
+        String errorMsg = HWPuSDKLibrary.INSTANCE.IVS_PU_GetErrorMsg(errorCode);
+        log.debug("返回码:{},返回信息:{}", errorCode.longValue(), errorMsg);
+    }
 
     @PostConstruct
     public void demo1() {
@@ -37,54 +38,21 @@ public class HuaWeiSdkApi {
         NativeLongByReference longNative = new NativeLongByReference();
         HWPuSDKLibrary.INSTANCE.IVS_PU_GetVersion(longNative);
         log.debug("SDK版本号:" + longNative.getValue().longValue());
-        pfGetEventInfoCallBack = new HWPuSDKLibrary.pfGetEventInfoCallBack() {
-            @Override
-            public NativeLong apply(PU_EVENT_COMMON arg) {
-                if (arg.enEventType == 2) {
-                    log.debug("发现设备主动注册");
-                    PU_EVENT_REGISTER registerEvent = new PU_EVENT_REGISTER(arg.getPointer());
-                    log.debug("设备类型:" + StrUtil.str(registerEvent.szDeviceType, "").trim());
-                    log.debug("设备ID:" + StrUtil.str(registerEvent.szDeviceId, "").trim().substring(0, 16));
-                    log.debug("设备IP地址:" + StrUtil.str(registerEvent.szDeviceIp, "").trim());
-                    boolean responseRegisterStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_ResponseDeviceRegister(arg.ulIdentifyID,
-                            new PU_DEVICE_REGISTER_RSP(0, new NativeLong(sdkPort)));
-                    log.debug("响应设备主动注册:" + responseRegisterStatus);
-                    if (!responseRegisterStatus) {
-                        printReturnMsg();
-                    }
-                    boolean loginStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_LoginByID(arg.ulIdentifyID, sdkUserName, sdkPassword);
-                    log.debug("设备登录状态:" + loginStatus);
-                    if (!loginStatus) {
-                        printReturnMsg();
-                    }
-                    PU_SYSTEM_TIME time = new PU_SYSTEM_TIME();
-                    boolean timeGetStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_GetDeviceTime(arg.ulIdentifyID, time);
-                    log.debug("时间获取状态:" + timeGetStatus);
-                    if (!timeGetStatus) {
-                        printReturnMsg();
-                    }
-                }
-                return new NativeLong(0);
-            }
-        };
+        //SDK注册事件回调
+        pfGetEventInfoCallBack = new EventInfoCallBack(sdkPort, sdkUserName, sdkPassword);
         boolean callBackBindStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_EventStatesCallBack(pfGetEventInfoCallBack);
         log.debug("注册事件回调函数绑定:" + callBackBindStatus);
         if (!callBackBindStatus) {
             printReturnMsg();
         }
+
+
     }
 
     @PreDestroy
     public void destroy() {
 //        HWPuSDKLibrary.INSTANCE.IVS_PU_Logout(deviceIdNative);
-//        printReturnMsg();
         HWPuSDKLibrary.INSTANCE.IVS_PU_Cleanup();
         printReturnMsg();
-    }
-
-    private void printReturnMsg() {
-        NativeLong errorCode = HWPuSDKLibrary.INSTANCE.IVS_PU_GetLastError();
-        String errorMsg = HWPuSDKLibrary.INSTANCE.IVS_PU_GetErrorMsg(errorCode);
-        log.debug("返回码:{},返回信息:{}", errorCode.longValue(), errorMsg);
     }
 }
---
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/EventInfoCallBack.java
@@ -0,0 +1,68 @@
+package com.summit.sdk.huawei.callback;
+
+import cn.hutool.core.util.StrUtil;
+import com.summit.sdk.huawei.HWPuSDKLibrary;
+import com.summit.sdk.huawei.PU_DEVICE_REGISTER_RSP;
+import com.summit.sdk.huawei.PU_EVENT_COMMON;
+import com.summit.sdk.huawei.PU_EVENT_REGISTER;
+import com.summit.sdk.huawei.PU_SYSTEM_TIME;
+import com.summit.sdk.huawei.PU_TIME;
+import com.summit.sdk.huawei.api.HuaWeiSdkApi;
+import com.sun.jna.NativeLong;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+
+
+@Slf4j
+public class EventInfoCallBack implements HWPuSDKLibrary.pfGetEventInfoCallBack {
+
+    private long sdkPort;
+    private String sdkUserName;
+    private String sdkPassword;
+
+    public EventInfoCallBack(long sdkPort, String sdkUserName, String sdkPassword) {
+        this.sdkPort = sdkPort;
+        this.sdkUserName = sdkUserName;
+        this.sdkPassword = sdkPassword;
+    }
+
+    @Override
+    public NativeLong apply(PU_EVENT_COMMON arg) {
+        if (arg.enEventType == 2) {
+            log.debug("发现设备主动注册");
+            PU_EVENT_REGISTER registerEvent = new PU_EVENT_REGISTER(arg.getPointer());
+            log.debug("设备类型:" + StrUtil.str(registerEvent.szDeviceType, "").trim());
+            log.debug("设备ID:" + StrUtil.str(registerEvent.szDeviceId, "").trim().substring(0, 16));
+            log.debug("设备IP地址:" + StrUtil.str(registerEvent.szDeviceIp, "").trim());
+            boolean responseRegisterStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_ResponseDeviceRegister(arg.ulIdentifyID,
+                    new PU_DEVICE_REGISTER_RSP(0, new NativeLong(sdkPort)));
+            log.debug("响应设备主动注册:" + responseRegisterStatus);
+            if (!responseRegisterStatus) {
+                HuaWeiSdkApi.printReturnMsg();
+            }
+            boolean loginStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_LoginByID(arg.ulIdentifyID, sdkUserName, sdkPassword);
+            log.debug("设备登录状态:" + loginStatus);
+            if (!loginStatus) {
+                HuaWeiSdkApi.printReturnMsg();
+            }
+            PU_SYSTEM_TIME time = new PU_SYSTEM_TIME();
+            boolean timeGetStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_GetDeviceTime(arg.ulIdentifyID, time);
+            log.debug("时间获取状态:" + timeGetStatus);
+            if (!timeGetStatus) {
+                HuaWeiSdkApi.printReturnMsg();
+            } else {
+                PU_TIME timeBytes = time.stSystime;
+                int year = Integer.valueOf(StrUtil.str(timeBytes.szYear, "").trim());
+                int month = Integer.valueOf(StrUtil.str(timeBytes.szMonth, "").trim());
+                int day = Integer.valueOf(StrUtil.str(timeBytes.szDay, "").trim());
+                int hour = Integer.valueOf(StrUtil.str(timeBytes.szHour, "").trim());
+                int minute = Integer.valueOf(StrUtil.str(timeBytes.szMinute, "").trim());
+                int second = Integer.valueOf(StrUtil.str(timeBytes.szSecond, "").trim());
+                int timeZone = time.lTimeZone.intValue();
+                String timeString = new DateTime(year, month, day, hour + timeZone, minute, second).toString("yyyy-MM-dd HH:mm:ss");
+                log.debug("设备时间:" + timeString);
+            }
+        }
+        return new NativeLong(0);
+    }
+}
