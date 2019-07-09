b4b7a7475080cd95c1f6c7f5595a8212e8bfeab6
LiuYuan <ly405653510@gmail.com>
2019-07-09 10:41:03 +0800

    commit

--- cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/EventInfoCallBack.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/EventInfoCallBack.java
@@ -145,46 +145,5 @@ public class EventInfoCallBack implements HWPuSDKLibrary.pfGetEventInfoCallBack 
             default:
                 break;
         }
-//        if (arg.enEventType == 2) {
-//            log.debug("发现设备主动注册");
-//            PU_EVENT_REGISTER registerEvent = new PU_EVENT_REGISTER(arg.getPointer());
-//            String deviceType = StrUtil.str(registerEvent.szDeviceType, "").trim();
-//            String deviceId = StrUtil.str(registerEvent.szDeviceId, "").trim().substring(0, 16);
-//            String deviceIp = StrUtil.str(registerEvent.szDeviceIp, "").trim();
-//            log.debug("设备类型:" + deviceType);
-//            log.debug("设备ID:" + deviceId);
-//            log.debug("设备IP地址:" + deviceIp);
-//            boolean responseRegisterStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_ResponseDeviceRegister(arg.ulIdentifyID,
-//                    new PU_DEVICE_REGISTER_RSP(0, new NativeLong(sdkPort)));
-//            log.debug("响应设备主动注册:" + responseRegisterStatus);
-//            if (!responseRegisterStatus) {
-//                HuaWeiSdkApi.printReturnMsg();
-//                return;
-//            }
-//            boolean loginStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_LoginByID(arg.ulIdentifyID, sdkUserName, sdkPassword);
-//            log.debug("设备登录状态:" + loginStatus);
-//            if (!loginStatus) {
-//                HuaWeiSdkApi.printReturnMsg();
-//                return;
-//            }
-//            deviceMap.put(deviceIp, arg.ulIdentifyID);
-//            PU_SYSTEM_TIME time = new PU_SYSTEM_TIME();
-//            boolean timeGetStatus = HWPuSDKLibrary.INSTANCE.IVS_PU_GetDeviceTime(arg.ulIdentifyID, time);
-//            log.debug("时间获取状态:" + timeGetStatus);
-//            if (!timeGetStatus) {
-//                HuaWeiSdkApi.printReturnMsg();
-//            } else {
-//                PU_TIME timeBytes = time.stSystime;
-//                int year = Integer.valueOf(StrUtil.str(timeBytes.szYear, "").trim());
-//                int month = Integer.valueOf(StrUtil.str(timeBytes.szMonth, "").trim());
-//                int day = Integer.valueOf(StrUtil.str(timeBytes.szDay, "").trim());
-//                int hour = Integer.valueOf(StrUtil.str(timeBytes.szHour, "").trim());
-//                int minute = Integer.valueOf(StrUtil.str(timeBytes.szMinute, "").trim());
-//                int second = Integer.valueOf(StrUtil.str(timeBytes.szSecond, "").trim());
-//                int timeZone = time.lTimeZone.intValue();
-//                String timeString = new DateTime(year, month, day, hour + timeZone, minute, second).toString("yyyy-MM-dd HH:mm:ss");
-//                log.debug("设备时间:" + timeString);
-//            }
-//        }
     }
 }
