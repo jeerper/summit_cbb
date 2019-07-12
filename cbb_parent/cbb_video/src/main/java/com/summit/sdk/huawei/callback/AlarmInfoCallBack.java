a377333a947f6539898cc1c8e975c71fa86692a7
LiuYuan <ly405653510@gmail.com>
2019-07-12 10:54:02 +0800

    完善解析摄像头告警信息

--- cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/AlarmInfoCallBack.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/AlarmInfoCallBack.java
@@ -1,17 +1,41 @@
 package com.summit.sdk.huawei.callback;
 
+import cn.hutool.core.util.StrUtil;
 import com.summit.sdk.huawei.HWPuSDKLibrary;
 import com.summit.sdk.huawei.PU_ALARM_REPORT;
+import com.summit.sdk.huawei.PU_TIME;
+import com.summit.sdk.huawei.model.AlarmType;
 import com.sun.jna.Pointer;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
 
 @Slf4j
 public class AlarmInfoCallBack implements HWPuSDKLibrary.pfGetAlarmInfoCallBack {
     @Override
     public void apply(PU_ALARM_REPORT pstAlarmReport, Pointer pUsrData) {
 
-        log.debug("告警设备ID:"+pstAlarmReport.ulDeviceId.longValue());
-        log.debug("告警类型:"+pstAlarmReport.enAlarmType);
-        log.debug("设备IP:"+pUsrData.getString(0));
+        log.debug("告警设备ID:" + pstAlarmReport.ulDeviceId.longValue());
+        AlarmType alarmType = AlarmType.codeOf(pstAlarmReport.enAlarmType);
+        if (alarmType == null) {
+            log.debug("未知告警类型:" + pstAlarmReport.enAlarmType);
+        } else {
+            log.debug("告警类型:" + alarmType.getAlarmDescription());
+        }
+        PU_TIME timeBytes = pstAlarmReport.stTime;
+        int year = Integer.valueOf(StrUtil.str(timeBytes.szYear, "").trim());
+        int month = Integer.valueOf(StrUtil.str(timeBytes.szMonth, "").trim());
+        int day = Integer.valueOf(StrUtil.str(timeBytes.szDay, "").trim());
+        int hour = Integer.valueOf(StrUtil.str(timeBytes.szHour, "").trim());
+        int minute = Integer.valueOf(StrUtil.str(timeBytes.szMinute, "").trim());
+        int second = Integer.valueOf(StrUtil.str(timeBytes.szSecond, "").trim());
+        String timeString = new DateTime(year, month, day, hour, minute, second).toString("yyyy-MM-dd HH:mm:ss");
+        log.debug("告警时间:" + timeString);
+        if (pstAlarmReport.enAction == 1) {
+            log.debug("告警动作:移除");
+        } else if (pstAlarmReport.enAction == 2) {
+            log.debug("告警动作:发生");
+        }
+        log.debug("告警ID:" + pstAlarmReport.ulAlarmId.longValue());
+        log.debug("设备IP:" + pUsrData.getString(0));
     }
 }
---
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/model/AlarmType.java
@@ -0,0 +1,32 @@
+package com.summit.sdk.huawei.model;
+
+public enum AlarmType {
+
+    FACE_DETECTION(10, "人脸检测"),
+    HUMAN_COUNT(119, "过线统计检测");
+
+    private int alarmCode;
+    private String alarmDescription;
+
+    AlarmType(int alarmCode, String alarmDescription) {
+        this.alarmCode = alarmCode;
+        this.alarmDescription = alarmDescription;
+    }
+
+    public static AlarmType codeOf(int alarmCode) {
+        for (AlarmType v : values()) {
+            if (v.alarmCode == alarmCode) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+    public int getAlarmCode() {
+        return alarmCode;
+    }
+
+    public String getAlarmDescription() {
+        return alarmDescription;
+    }
+}
