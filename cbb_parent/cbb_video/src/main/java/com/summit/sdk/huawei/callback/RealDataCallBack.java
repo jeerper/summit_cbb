1c466fb3b6efe5952f0437078e2adc0c1362b457
LiuYuan <ly405653510@gmail.com>
2019-07-15 20:55:14 +0800

    commit

--- cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/RealDataCallBack.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/RealDataCallBack.java
@@ -1,5 +1,6 @@
 package com.summit.sdk.huawei.callback;
 
+import cn.hutool.core.convert.Convert;
 import com.summit.sdk.huawei.HWPuSDKLibrary;
 import com.summit.sdk.huawei.PU_META_DATA;
 import com.summit.sdk.huawei.PU_UserData;
@@ -21,6 +22,7 @@ public class RealDataCallBack implements HWPuSDKLibrary.pfRealDataCallBack {
         PU_UserData[] userData = (PU_UserData[]) data.pstMetaUserData.toArray(data.usValidNumber);
         for (PU_UserData userDataEntity : userData) {
             log.debug("eType:" + userDataEntity.eType);
+            log.debug("eType-Hex:" + Convert.toHex(Convert.intToBytes(userDataEntity.eType)));
         }
         log.debug("设备IP:" + pUsrData.getString(0));
         HWPuSDKLibrary.INSTANCE.IVS_User_FreeMetaData(pu_meta_data_pointer_pointer);
