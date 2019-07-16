e73e33499f67fa0762ab21df32c2ac811d8f1fae
LiuYuan <ly405653510@gmail.com>
2019-07-16 19:34:47 +0800

    人脸信息数据解析

--- cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/RealDataCallBack.java
+++ cbb_parent/cbb_video/src/main/java/com/summit/sdk/huawei/callback/RealDataCallBack.java
@@ -15,19 +15,19 @@ public class RealDataCallBack implements HWPuSDKLibrary.pfRealDataCallBack {
 
     @Override
     public void apply(Pointer szBuffer, NativeLong lSize, Pointer pUsrData) {
-        log.debug("设备IP:" + pUsrData.getString(0));
-        procBuffer(szBuffer,lSize,HWPuSDKLibrary.LAYER_TWO_TYPE.COMMON);
-        procBuffer(szBuffer,lSize,HWPuSDKLibrary.LAYER_TWO_TYPE.TARGET);
+//        log.debug("设备IP:" + pUsrData.getString(0));
+        procBuffer(szBuffer, lSize, HWPuSDKLibrary.LAYER_TWO_TYPE.COMMON);
+        procBuffer(szBuffer, lSize, HWPuSDKLibrary.LAYER_TWO_TYPE.TARGET);
     }
 
-    private void procBuffer(Pointer szBuffer, NativeLong lSize,int layerTwoType){
+    private void procBuffer(Pointer szBuffer, NativeLong lSize, int layerTwoType) {
         PointerByReference pu_meta_data_pointer_pointer = new PointerByReference(Pointer.NULL);
         HWPuSDKLibrary.INSTANCE.IVS_User_GetMetaData(szBuffer, lSize, layerTwoType, pu_meta_data_pointer_pointer);
         procMetaData(pu_meta_data_pointer_pointer);
         HWPuSDKLibrary.INSTANCE.IVS_User_FreeMetaData(pu_meta_data_pointer_pointer);
     }
 
-    private void procMetaData(PointerByReference pu_meta_data_pointer_pointer){
+    private void procMetaData(PointerByReference pu_meta_data_pointer_pointer) {
         PU_META_DATA data = Structure.newInstance(PU_META_DATA.class, pu_meta_data_pointer_pointer.getValue());
         data.read();
         PU_UserData[] userData = (PU_UserData[]) data.pstMetaUserData.toArray(data.usValidNumber);
@@ -35,8 +35,19 @@ public class RealDataCallBack implements HWPuSDKLibrary.pfRealDataCallBack {
             switch (userDataEntity.eType) {
                 case HWPuSDKLibrary.LAYER_THREE_TYPE.FACE_INFO:
                     log.debug("================人脸信息业务处理=================");
-                    log.debug(StrUtil.str(userDataEntity.unMetaData.stFaceInfo.name,"").trim());
-
+                    log.debug("名字:" + StrUtil.str(userDataEntity.unMetaData.stFaceInfo.name, "").trim());
+                    if (userDataEntity.unMetaData.stFaceInfo.iGender == 1) {
+                        log.debug("性别:女");
+                    } else if (userDataEntity.unMetaData.stFaceInfo.iGender == 2) {
+                        log.debug("性别:男");
+                    } else {
+                        log.debug("性别:未知");
+                    }
+                    log.debug("生日:" + StrUtil.str(userDataEntity.unMetaData.stFaceInfo.birthday, "").trim());
+                    log.debug("省级:" + StrUtil.str(userDataEntity.unMetaData.stFaceInfo.province, "").trim());
+                    log.debug("地市:" + StrUtil.str(userDataEntity.unMetaData.stFaceInfo.city, "").trim());
+                    log.debug("证件类型:" + userDataEntity.unMetaData.stFaceInfo.iCardType);
+                    log.debug("证件号:" + StrUtil.str(userDataEntity.unMetaData.stFaceInfo.cardID, "").trim());
                     break;
                 default:
 //                    log.debug("未知数据类型eType-Hex: 0x" + Convert.toHex(Convert.intToBytes(userDataEntity.eType)).toUpperCase());
