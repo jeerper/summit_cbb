d3988be8fbdfb0014c49ae3a84ba8169fbb778c5
liuyonghong <liuyonghong_lyh@163.com>
2019-08-07 14:25:25 +0800

    把角色系统管理员改成ROLE_SUPERUSER

---
+++ cbb_parent/cbb_userauth/src/main/resources/db/V1.6__data.sql
@@ -0,0 +1,6 @@
+ update sys_role set code='ROLE_SUPERUSER' where code='ROLE_1489581392504';
+ update sys_role_function set ROLE_CODE='ROLE_SUPERUSER' where ROLE_CODE='ROLE_1489581392504';
+ update sys_user_role set ROLE_CODE='ROLE_SUPERUSER' where ROLE_CODE='ROLE_1489581392504';
+ 
+ 
+ 
