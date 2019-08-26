f5c16653ed46f4959804a9340d765c954ab29c7b
hegangdong <1ac2dc36de8b40a0aa4c37fc3ea016ff@huawei.com>
2019-08-26 14:28:43 +0800

    排查问题；

--- cbb_parent/cbb_userauth/src/main/java/com/summit/repository/UserRepository.java
+++ cbb_parent/cbb_userauth/src/main/java/com/summit/repository/UserRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
