416a60146b081ca6fc68d6e4338932bab42f71ca
liuyuan <405653510@qq.com>
2020-02-11 21:26:21 +0800

    增加用户登录验证成功事件监听器

---
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/handle/SummitAuthenticationSuccessEventHandler.java
@@ -0,0 +1,52 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.summit.handle;
+
+import cn.hutool.core.collection.CollUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 用户登录验证成功事件处理器
+ * @author liuyuan
+ */
+@Slf4j
+@Component
+public class SummitAuthenticationSuccessEventHandler implements ApplicationListener<AuthenticationSuccessEvent> {
+
+    @Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        Authentication authentication = (Authentication) event.getSource();
+        if (CollUtil.isEmpty(authentication.getAuthorities())) {
+            return;
+        }
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        log.debug(request.getRemoteAddr());
+
+    }
+
+
+}
