1d8dab3077b7f02ac9c096376d97de6ad121d5cd
liuyuan <405653510@qq.com>
2020-02-12 19:47:38 +0800

    token生成器增加附加属性ID

--- cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/handle/SummitAuthenticationSuccessEventHandler.java
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/handle/SummitAuthenticationSuccessEventHandler.java
@@ -5,10 +5,11 @@ import cn.hutool.core.util.IdUtil;
 import com.summit.model.user.UserBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -24,7 +25,7 @@ import java.util.List;
  * @author liuyuan
  */
 @Slf4j
-@Component
+//@Component
 public class SummitAuthenticationSuccessEventHandler implements ApplicationListener<AuthenticationSuccessEvent> {
 
     @Override
@@ -33,31 +34,42 @@ public class SummitAuthenticationSuccessEventHandler implements ApplicationListe
         if (CollUtil.isEmpty(authentication.getAuthorities())) {
             return;
         }
-        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = requestAttributes.getRequest();
-        UserBean userBean = (UserBean) authentication.getPrincipal();
 
-        String loginId = IdUtil.objectId();
-        String loginUserName = userBean.getUserName();
-        String loginIp = "";
-        if ("0:0:0:0:0:0:0:1".equals(request.getRemoteAddr())) {
-            try {
-                loginIp = InetAddress.getLocalHost().toString();
-                int computNameIndex = loginIp.indexOf("/");
-                if (computNameIndex != -1) {
-                    loginIp = loginIp.substring(computNameIndex + 1);
+        if(authentication instanceof UsernamePasswordAuthenticationToken){
+
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken= (UsernamePasswordAuthenticationToken)authentication;
+            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest request = requestAttributes.getRequest();
+            UserBean userBean = (UserBean) authentication.getPrincipal();
+
+            String loginId = IdUtil.objectId();
+            String loginUserName = userBean.getUserName();
+            String loginIp = "";
+            if ("0:0:0:0:0:0:0:1".equals(request.getRemoteAddr())) {
+                try {
+                    loginIp = InetAddress.getLocalHost().toString();
+                    int computNameIndex = loginIp.indexOf("/");
+                    if (computNameIndex != -1) {
+                        loginIp = loginIp.substring(computNameIndex + 1);
+                    }
+                } catch (UnknownHostException e) {
+                    log.error("获取ip失败！" + e.getMessage());
                 }
-            } catch (UnknownHostException e) {
-                log.error("获取ip失败！" + e.getMessage());
+            }else{
+                loginIp = request.getRemoteAddr();
             }
-        }else{
-            loginIp = request.getRemoteAddr();
+            Date loginLogCreateTime = new Date();
+            List<GrantedAuthority> authorities = (List<GrantedAuthority>) authentication.getAuthorities();
+
+
+            //TODO:请求用户组件记录用户登录日志接口
+
+
         }
-        Date loginLogCreateTime = new Date();
-        List<GrantedAuthority> authorities = (List<GrantedAuthority>) authentication.getAuthorities();
+        else if (authentication instanceof OAuth2Authentication){
 
+        }
 
-        //TODO:请求用户组件记录用户登录日志接口
 
     }
 
