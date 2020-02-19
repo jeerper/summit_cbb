725bf5693c29117a077fa3d00b2b18daefc829ed
liuyuan <405653510@qq.com>
2020-02-19 10:01:46 +0800

    增加登陆日志查询接口

---
+++ cbb_parent/cbb_userauth/src/main/java/com/summit/dao/entity/LoginLogInfo.java
@@ -0,0 +1,22 @@
+package com.summit.dao.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+public class LoginLogInfo {
+    private String userName;
+    private String name;
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+    private Date loginTime;
+    private int onlineTime;
+    private String callerIP;
+
+
+
+
+}
