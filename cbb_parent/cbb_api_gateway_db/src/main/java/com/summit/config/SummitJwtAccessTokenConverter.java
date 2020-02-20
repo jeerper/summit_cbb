121af3280c3ddbcba456411449fb37550674acd5
liuyuan <405653510@qq.com>
2020-02-20 14:10:32 +0800

    登陆返回结果中加入登陆账号

--- cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/SummitJwtAccessTokenConverter.java
+++ cbb_parent/cbb_api_gateway_db/src/main/java/com/summit/config/SummitJwtAccessTokenConverter.java
@@ -2,6 +2,7 @@ package com.summit.config;
 
 
 import com.summit.common.constant.CommonConstant;
+import com.summit.model.user.UserBean;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -15,6 +16,10 @@ public class SummitJwtAccessTokenConverter extends JwtAccessTokenConverter {
    @Override
    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
        final Map<String, Object> additionalInfo = new HashMap<>(2);
+       UserBean user = (UserBean) authentication.getUserAuthentication().getPrincipal();
+       if (user != null) {
+           additionalInfo.put("username", user.getUsername());
+       }
        additionalInfo.put("license", CommonConstant.LICENSE);
        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
        return super.enhance(accessToken,  authentication);
