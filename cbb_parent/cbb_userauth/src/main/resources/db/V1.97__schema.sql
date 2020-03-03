a13dbb15decd0f2e54dda6419ed276ac1faf7ad6
qianyy <2997272453@qqcom>
2020-03-03 10:05:50 +0800

    用户表和部门表增加审核字段

--- cbb_parent/cbb_userauth/src/main/resources/db/V1.97__schema.sql
+++ cbb_parent/cbb_userauth/src/main/resources/db/V1.97__schema.sql
@@ -1,1 +1,2 @@
 ﻿alter table sys_user add column isAudited varchar(10) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '审核是否通过(0：发起审核，1：通过，2：不通过)';
+alter table sys_dept add column isAudited varchar(10) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '审核是否通过(0：发起审核，1：通过，2：不通过)';
