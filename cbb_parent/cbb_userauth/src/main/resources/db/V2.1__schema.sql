5410d6648ad6c6936da4e465067fc3fc647b9690
qianyy <2997272453@qqcom>
2020-03-10 13:39:46 +0800

    修改bug

--- cbb_parent/cbb_userauth/src/main/resources/db/V2.1__schema.sql
+++ cbb_parent/cbb_userauth/src/main/resources/db/V2.1__schema.sql
@@ -1,2 +1,2 @@
-﻿alter table sys_user_auth add column remark varchar(500) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '备注';
-alter table sys_dept_auth add column remark varchar(500) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '备注';
+﻿alter table sys_user_auth drop column remark;
+alter table sys_dept_auth drop column remark;
