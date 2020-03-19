45a7084a65eee806cb08a4ea661ba3c1f953a243
qianyy <2997272453@qqcom>
2020-03-19 15:47:20 +0800

    记录表增加字段

---
+++ cbb_parent/cbb_userauth/src/main/resources/db/V2.1.8__schema.sql
@@ -0,0 +1,5 @@
+﻿alter table sys_dept_record add column deptId varchar(50) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '部门id';
+alter table sys_dept_auth add column deptRecord_id varchar(50) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '部门记录表id';
+alter table sys_user_record drop primary key;
+alter table sys_user_record add column id varchar(50) primary key;
+alter table sys_user_auth add column userRecord_id varchar(50) CHARACTER SET utf8 COLLATE utf8_bin COMMENT '用户记录表id';
