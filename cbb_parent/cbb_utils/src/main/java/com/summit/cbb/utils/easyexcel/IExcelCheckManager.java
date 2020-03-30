7574bd283763e8c392ef45f0d4d295e90cd00601
maoyuxuan <maoyx@summit.com.cn>
2020-03-30 15:09:34 +0800

    easyexcel工具包新增依赖

---
+++ cbb_parent/cbb_utils/src/main/java/com/summit/cbb/utils/easyexcel/IExcelCheckManager.java
@@ -0,0 +1,17 @@
+package com.summit.cbb.utils.easyexcel;
+
+import com.summit.cbb.utils.easyexcel.entity.ExcelCheckResult;
+
+import java.util.List;
+
+/**
+ * excel业务校验接口
+ *
+ * @Author maoyuxuan
+ * @Date 2020/3/28 22:10
+ **/
+public interface IExcelCheckManager<T> {
+
+    ExcelCheckResult checkImportExcel(List<T> checks);
+
+}
