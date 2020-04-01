c072c7f623fd01df285334cc2ed7e2e6e27c9701
maoyx <maoyx@summit.com.cn>
2020-04-01 19:41:33 +0800

    1.修改全局异常拦截打印的日志内容 2.前端提示信息增加自定义方式

--- cbb_parent/cbb_common/src/main/java/com/summit/common/advice/GlobalExceptionControllerAdvice.java
+++ cbb_parent/cbb_common/src/main/java/com/summit/common/advice/GlobalExceptionControllerAdvice.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * 全局异常处理捕捉类
@@ -30,9 +32,14 @@ public class GlobalExceptionControllerAdvice {
     public RestfulEntityBySummit<?> handleException(HttpServletRequest req, HttpServletResponse rsp, Exception e) {
         if (e instanceof CommonException) {//捕获CommonException异常
             CommonException ce = (CommonException)e;
+            if (ce.getStatusCode() == null){
+                return ResultBuilder.buildError(ResponseCodeEnum.CODE_9999,ce.getErrorMsg(),null);
+            }
             return ResultBuilder.buildError(ce.getStatusCode());
         } else {//捕获其他异常
-            log.error(e.getMessage(), e.fillInStackTrace());
+            StringWriter sw= new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            log.error(sw.toString());
             return ResultBuilder.buildError(ResponseCodeEnum.CODE_9999);
         }
     }
