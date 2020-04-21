45fa029d08711f0993ea46e5c6c3725857485065
qianyy <2997272453@qqcom>
2020-04-21 10:40:12 +0800

    用户审核表和用户记录表增加字段

---
+++ cbb_parent/cbb_userauth/src/main/resources/db/V2.2__schema.sql
@@ -0,0 +1,2 @@
+﻿alter table sys_user_record add column state int(11) NULL DEFAULT NULL  COMMENT '是否删除  0已删除1正常';
+alter table sys_user_auth add column state_auth int(11) NULL DEFAULT NULL  COMMENT '是否删除  0已删除1正常';
