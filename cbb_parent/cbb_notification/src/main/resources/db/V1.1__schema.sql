0888a24a34f122a53eb67223c3e0114de11780f8
jeerper <297272453@qq.com>
2020-12-16 16:57:25 +0800

    第一次提交

--- cbb_parent/cbb_notification/src/main/resources/db/V1.1__schema.sql
+++ cbb_parent/cbb_notification/src/main/resources/db/V1.1__schema.sql
@@ -6,16 +6,16 @@ drop table if exists msg_email;
 /*==============================================================*/
 create table msg_email 
 (
-   email_id             varchar(32)      not null     comment '�ʼ����',
-   email_name           varchar(64)      null         comment '�ʼ�����',
-   send_to              varchar(64)      not null     comment '�����ߵ�ַ',
-   email_title          varchar(256)     not null     comment '�ʼ�����',
-   email_content        text             not null     comment '�ʼ�����',
-   send_state           integer          null         comment '����״̬',
-   create_time          datetime         null         comment '����ʱ��',
-   update_time          datetime         null         comment '����ʱ��',
+   email_id             varchar(32)      not null     comment  '',
+   email_name           varchar(64)      null         comment  '',
+   send_to              varchar(64)      not null     comment  '',
+   email_title          varchar(256)     not null     comment  '',
+   email_content        text             not null     comment  '',
+   send_state           integer          null         comment  '' ,
+   create_time          datetime         null         comment  '',
+   update_time          datetime         null         comment  '',
    primary key (email_id)
-) ENGINE=InnoDB default CHARSET=utf8 comment '�ʼ���¼��';
+) ENGINE=InnoDB default CHARSET=utf8 comment '';
 
 
 
@@ -25,17 +25,17 @@ drop table if exists msg_sms;
 /*==============================================================*/
 create table msg_sms 
 (
-   sms_id            varchar(32)     not null   comment '���ű��',
-   template_id       varchar(32)     not null   comment 'ģ��id',
-   res_phone         varchar(20)     not null   comment '�������ֻ���',
-   biz_id            varchar(64)     null       comment '���ͻ�ִID',
-   sms_signname      varchar(32)     not null   comment '����ǩ��',
-   sms_content       text            not null   comment '��������',
-   send_state        integer         null       comment '����״̬',
-   create_time       datetime        null       comment '����ʱ��',
-   update_time       datetime        null       comment '����ʱ��',
+   sms_id            varchar(32)     not null   comment  '',
+   template_id       varchar(32)     not null   comment  '',
+   res_phone         varchar(20)     not null   comment  '',
+   biz_id            varchar(64)     null       comment  '',
+   sms_signname      varchar(32)     not null   comment  '',
+   sms_content       text            not null   comment  '',
+   send_state        integer         null       comment  '',
+   create_time       datetime        null       comment  '',
+   update_time       datetime        null       comment  '',
    primary key (sms_id)
-) ENGINE=InnoDB default CHARSET=utf8 comment '���ż�¼��';
+) ENGINE=InnoDB default CHARSET=utf8 comment  '';
 
 
 
@@ -46,14 +46,14 @@ drop table if exists sms_template;
 /*==============================================================*/
 create table sms_template 
 (
-   template_id          varchar(32)   not null   comment 'ģ����',
-   template_content     varchar(255)   null       comment 'ģ������',
-   template_code        varchar(32)   not null   comment 'ģ���',
-   template_type        varchar(16)   null       comment 'ģ������',
-   create_time          datetime      null       comment '����ʱ��',
-   update_time          datetime      null       comment '����ʱ��',
+   template_id          varchar(32)   not null   comment  '',
+   template_content     varchar(255)   null       comment  '',
+   template_code        varchar(32)   not null   comment  '',
+   template_type        varchar(16)   null       comment  '',
+   create_time          datetime      null       comment  '',
+   update_time          datetime      null       comment  '',
    primary key (template_id)
-) ENGINE=InnoDB default CHARSET=utf8 comment '����ģ���';
+) ENGINE=InnoDB default CHARSET=utf8 comment '';
 
 
 
